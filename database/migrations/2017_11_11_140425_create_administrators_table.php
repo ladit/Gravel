948397ef11b90ec3c20a2b0c466cd6f49693c2e7
ladit <ladit@qq.com>
2017-11-14 22:50:40 +0800

    refactor(database): 密码与 Token 字段类型长度更新
    - 适应密码和 Token 的生成方式

--- database/migrations/2017_11_11_140425_create_administrators_table.php
+++ database/migrations/2017_11_11_140425_create_administrators_table.php
@@ -17,9 +17,9 @@ class CreateAdministratorsTable extends Migration
             $table->increments('id')->unsigned()->comment('管理员 id');
             $table->timestamps();
             $table->string('account', 30)->unique()->comment('用户名');
-            $table->char('password', 128)->comment('密码');
-            $table->string('access_token', 255)->default('')->comment('access token');
-            $table->string('access_refresh_token', 255)->default('')->comment('access refresh token');
+            $table->string('password')->comment('密码');
+            $table->string('access_token')->default('')->comment('access token');
+            $table->string('access_refresh_token')->default('')->comment('access refresh token');
             $table->timestamp('access_token_expires_in')->default($now = date("Y-m-d H:i:s"))->comment('access token 过期时间');
             $table->index(['id', 'account']);
         });
