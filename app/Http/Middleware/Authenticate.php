d6a67ef6dbad70310fac8d6112fed16837ce8b18
ladit <ladit@qq.com>
2017-11-16 11:21:17 +0800

    chore(middleware): 构建鉴权中间件逻辑

--- app/Http/Middleware/Authenticate.php
+++ app/Http/Middleware/Authenticate.php
@@ -18,32 +18,40 @@ class Authenticate
      */
     public function handle(Request $request, Closure $next)
     {
-        // body 内容非 JSON 则返回 400 错误
-        if (!$request->isJson()) {
+        // body 内容非 JSON、header 无 Authorization 字段、路由路径错误，返回 400
+        $requestSegments = $request->segments();
+        //
+        if (!$request->isJson() or !$request->header('Authorization')
+            or !is_numeric($requestSegments[2])) {
             return response()->json([
-                'error_code' => 400
+                'error_code' => 400,
+                'error_message' => 'Body should be json, router should be right, require authorization.'
             ]);
         }
 
-        // 验证路由路径，访问错误则返回 404
-        $requestSegments = $request->segments();
-        if ($requestSegments[0] != 'api' or $requestSegments[1] != 'users' or !is_numeric($requestSegments[2])) {
+        $user = $request->user;
+        $access_token = $request->header('Authorization');
+
+        if (!$user) {
             return response()->json([
-                'error_code' => 400
+                'error_code' => 403,
+                'error_message' => 'User not exist.'
+            ]);
+        }
+
+        // Token 过期
+        if (strtotime($user->access_token_expires_in) < time()) {
+            return response()->json([
+                'error_code' => 403,
+                'error_message' => 'Access token expired.'
             ]);
         }
 
         // Token 验证
-        try {
-            $user = User::findOrFail($requestSegments[2]);
-        } catch (ModelNotFoundException $e) {
+        if ($access_token != $user->access_token) {
             return response()->json([
-                'error_code' => 403
-            ]);
-        }
-        if ($request->header('Authorization') != $user->access_token) {
-            return response()->json([
-                'error_code' => 401
+                'error_code' => 401,
+                'error_message' => 'Wrong access token.'
             ]);
         }
 
