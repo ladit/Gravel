b0d07ad41b896d22734206cbb14328c62334c706
ladit <ladit@qq.com>
2017-11-17 17:03:16 +0800

    refactor(middleware):  重构鉴权中间件
    - 修复了 GET 方式返回错误 400 的 bug
    - 错误信息更加有针对性
    - 配合路由改动，验证 refresh token

--- app/Http/Middleware/Authenticate.php
+++ app/Http/Middleware/Authenticate.php
@@ -18,19 +18,33 @@ class Authenticate
      */
     public function handle(Request $request, Closure $next)
     {
-        // body 内容非 JSON、header 无 Authorization 字段、路由路径错误，返回 400
-        $requestSegments = $request->segments();
-        if (!$request->isJson() or !$request->header('Authorization')
-            or !is_numeric($requestSegments[2])) {
+        // 非 GET 方式，body 内容非 JSON，返回 400
+        if ($request->method() != 'GET' and !$request->isJson()) {
             return response()->json([
                 'error_code' => 400,
-                'error_message' => 'Body should be json, router should be right, require authorization.'
+                'error_message' => 'If method is not GET, body should be json.'
             ]);
         }
 
-        $user = $request->user;
-        $access_token = $request->header('Authorization');
+        // header 无 Authorizations 字段，返回 400
+        $token = $request->header('Authorizations');
+        if (!$token) {
+            return response()->json([
+                'error_code' => 400,
+                'error_message' => 'Require Authorizations in header.'
+            ]);
+        }
 
+        // 路由中用户错误，返回 400
+        if (!is_numeric($request->segment(3))) {
+            return response()->json([
+                'error_code' => 400,
+                'error_message' => 'User ID error.'
+            ]);
+        }
+
+        // 用户不存在，返回 403
+        $user = $request->user;
         if (!$user) {
             return response()->json([
                 'error_code' => 403,
@@ -38,7 +52,24 @@ class Authenticate
             ]);
         }
 
-        // Token 过期
+        // Refresh token 过期（14天过期）
+        if ($request->segment(3) == 'access_token') {
+            if (strtotime($user->access_token_expires_in) + config('app.token_expires_seconds') < time()) {
+                return response()->json([
+                    'error_code' => 403,
+                    'error_message' => 'Refresh token expired.'
+                ]);
+            }
+
+            // Refresh token 验证
+            if ($token != $user->access_refresh_token) {
+                return response()->json([
+                    'error_code' => 401,
+                    'error_message' => 'Wrong access refresh token.'
+                ]);
+            }
+        }
+
         if (strtotime($user->access_token_expires_in) < time()) {
             return response()->json([
                 'error_code' => 403,
@@ -47,7 +78,7 @@ class Authenticate
         }
 
         // Token 验证
-        if ($access_token != $user->access_token) {
+        if ($token != $user->access_token) {
             return response()->json([
                 'error_code' => 401,
                 'error_message' => 'Wrong access token.'
