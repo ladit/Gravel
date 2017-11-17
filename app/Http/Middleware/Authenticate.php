6c392db7026e13fb72dd4fa14fc02bbf012b2647
ladit <ladit@qq.com>
2017-11-18 03:03:58 +0800

    refactor(middleware): 重构鉴权中间件
    - 错误代码与提示更新
    - 加入 Refresh token 验证
    - 访问有文章 id 的路由，检验文章 id 错误
    - 访问有记录 id 的路由，检验记录 id 错误，且验证是否匹配

--- app/Http/Middleware/Authenticate.php
+++ app/Http/Middleware/Authenticate.php
@@ -4,6 +4,7 @@ namespace App\Http\Middleware;
 
 use Closure;
 use App\User;
+use App\Note;
 use Illuminate\Http\Request;
 use Illuminate\Database\Eloquent\ModelNotFoundException;
 
@@ -18,7 +19,7 @@ class Authenticate
      */
     public function handle(Request $request, Closure $next)
     {
-        // 非 GET 方式，body 内容非 JSON，返回 400
+        // 非 GET 方式，header 中 Content-Type 不为 application/json（body 内容非 JSON），返回 400
         if ($request->method() != 'GET' and !$request->isJson()) {
             return response()->json([
                 'error_code' => 400,
@@ -35,10 +36,12 @@ class Authenticate
             ]);
         }
 
-        // 路由中用户错误，返回 400
-        if (!is_numeric($request->segment(3))) {
+        $requestSegments = $request->segments();
+
+        // 路由中用户 id 错误，返回 404
+        if (!is_numeric($requestSegments[2])) {
             return response()->json([
-                'error_code' => 400,
+                'error_code' => 404,
                 'error_message' => 'User ID error.'
             ]);
         }
@@ -47,20 +50,12 @@ class Authenticate
         $user = $request->user;
         if (!$user) {
             return response()->json([
-                'error_code' => 403,
-                'error_message' => 'User not exist.'
+                'error_code' => 404,
+                'error_message' => 'User does not exist.'
             ]);
         }
 
-        // Refresh token 过期（14天过期）
-        if ($request->segment(3) == 'access_token') {
-            if (strtotime($user->access_token_expires_in) + config('app.token_expires_seconds') < time()) {
-                return response()->json([
-                    'error_code' => 403,
-                    'error_message' => 'Refresh token expired.'
-                ]);
-            }
-
+        if ($requestSegments[3] == 'access_token') {
             // Refresh token 验证
             if ($token != $user->access_refresh_token) {
                 return response()->json([
@@ -68,20 +63,58 @@ class Authenticate
                     'error_message' => 'Wrong access refresh token.'
                 ]);
             }
+
+            // 检查 Refresh token 过期（14 天过期）
+            if (strtotime($user->access_token_expires_in)
+                + config('app.token_expires_seconds') < time()) {
+                User::refreshToken($user);
+                return response()->json([
+                    'error_code' => 403,
+                    'error_message' => 'Refresh token expired.'
+                ]);
+            }
+        } else {
+            // Token 验证
+            if ($token != $user->access_token) {
+                return response()->json([
+                    'error_code' => 401,
+                    'error_message' => 'Wrong access token.'
+                ]);
+            }
+
+            // 检查 Access token 过期（7 天过期）
+            if (strtotime($user->access_token_expires_in) < time()) {
+                return response()->json([
+                    'error_code' => 403,
+                    'error_message' => 'Access token expired.'
+                ]);
+            }
         }
 
-        if (strtotime($user->access_token_expires_in) < time()) {
-            return response()->json([
-                'error_code' => 403,
-                'error_message' => 'Access token expired.'
-            ]);
+        // 路由中记录 id 错误，返回 404
+        if ($requestSegments[3] == 'notes' and isset($requestSegments[4])) {
+            if (!is_numeric($requestSegments[4])) {
+                return response()->json([
+                    'error_code' => 404,
+                    'error_message' => 'Note ID error.'
+                ]);
+            }
+            $note = $request->note;
+            if ($note->user->id != $user->id) {
+                return response()->json([
+                    'error_code' => 404,
+                    'error_message' => 'Note ID error.'
+                ]);
+            }
         }
 
-        // Token 验证
-        if ($token != $user->access_token) {
+        // 路由中文章 id 错误，返回 404
+        if ($requestSegments[3] == 'articles'
+            and isset($requestSegments[4])
+            and !is_numeric($requestSegments[4])) {
             return response()->json([
-                'error_code' => 401,
-                'error_message' => 'Wrong access token.'
+                'error_code' => 404,
+                'error_message' => 'Article ID error.'
             ]);
         }
 
